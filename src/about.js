096cc6b8016c43fdbbe72e6ad6dcfe9735297f2b
idgm5 <idgm@pm.me>
2020-06-16 21:29:17 -0500

    Finish about page including CSS

--- src/about.js
+++ src/about.js
@@ -8,10 +8,30 @@ const about = () => {
   const paraSecond = document.createElement("P");
   const paraThird = document.createElement("P");
 
+  //About images
   const divImg = document.createElement("DIV");
   const divImgMain = document.createElement("DIV");
   const divImgCollection = document.createElement("DIV");
+  const imgFirst = document.createElement("IMG");
+  const imgSecond = document.createElement("IMG");
+  const imgThird = document.createElement("IMG");
 
+  divAbout.setAttribute("class", "about");
+  divImg.setAttribute("class", "img-container");
+  divImgMain.setAttribute("class", "img-main");
+  divImgCollection.setAttribute("class", "img-collection");
+
+  imgFirst.setAttribute("src", "./assets/cucumbers.png");
+  imgFirst.setAttribute("class", "about-main-img");
+  imgSecond.setAttribute("src", "./assets/cow.png");
+  imgSecond.setAttribute("class", "about-collection-img");
+  imgThird.setAttribute("src", "./assets/bread.png");
+  imgThird.setAttribute("class", "about-collection-img");
+
+  divImgMain.appendChild(imgFirst);
+  divImgCollection.append(imgSecond, imgThird);
+  divImg.append(divImgMain, divImgCollection);
+  //About page
   h2.innerHTML = "The Journey"
   paraFirst.innerHTML = "On May 4th, 2005 we opened the doors to Coconut Mojo. The culmination of years of dreaming and imagination, Coconut Mojo was a minimalist modern vision that put the focal point on the cuisine and dining as an experience."
   paraSecond.innerHTML = "On 2018 Coconut Mojo was awarded a Michelin Star and was selected as the second best restaurant in the world for 2018 by the Elite Traveler Magazine."
