4986f2cfa1b4db1f59e5ba959da65ebd4cb9b4ce
idgm5 <idgm@pm.me>
2020-06-17 11:37:30 -0500

    Add linters for JS and CSS, fix most lints from CSS and JS files

--- src/footer.js
+++ src/footer.js
@@ -1,14 +1,14 @@
 
 const footer = () => {
-  const div = document.getElementById("mainContent");
-  const divfooter = document.createElement("footer");
-  const link = document.createElement("a");
+  const div = document.getElementById('mainContent');
+  const divfooter = document.createElement('footer');
+  const link = document.createElement('a');
 
-  link.setAttribute("href", "https://github.com/idgm5");
-  link.innerHTML="Build with Vanilla JS by @idgm5";
+  link.setAttribute('href', 'https://github.com/idgm5/restaurantJS');
+  link.innerHTML = 'Build with Vanilla JS by @idgm5';
 
   divfooter.appendChild(link);
   div.appendChild(divfooter);
-}
+};
 
 export default footer;
--- src/index.js
+++ src/index.js
@@ -1,66 +1,67 @@
-var _ = require('lodash');
-import menu from './menu.js';
-import about from './about.js';
-import contact from './contact.js';
-import footer from './footer.js';
+import menu from './menu';
+import about from './about';
+import contact from './contact';
+import footer from './footer';
 
-//navbar
+const _ = require('lodash');
+
+// navbar
 const div = document.getElementById('content');
 const divMain = document.createElement('DIV');
-divMain.setAttribute("id", "mainContent");
+divMain.setAttribute('id', 'mainContent');
 
 const nav = document.createElement('NAV');
 const h1 = document.createElement('H1');
-h1.innerHTML = "Coconut Mojo";
+h1.innerHTML = 'Coconut Mojo';
 const navContainer = document.createElement('DIV');
 
-//navbar items
+// navbar items
 const navList = document.createElement('UL');
 const navListMenu = document.createElement('LI');
-navListMenu.innerHTML = "Menu";
-navListMenu.setAttribute("id", "navListMenu");
+navListMenu.innerHTML = 'Menu';
+navListMenu.setAttribute('id', 'navListMenu');
 const navListAbout = document.createElement('LI');
-navListAbout.innerHTML = "About";
-navListAbout.setAttribute("id", "navListAbout");
+navListAbout.innerHTML = 'About';
+navListAbout.setAttribute('id', 'navListAbout');
 const navListContact = document.createElement('LI');
-navListContact.innerHTML = "Contact";
-navListContact.setAttribute("id", "navListContact");
+navListContact.innerHTML = 'Contact';
+navListContact.setAttribute('id', 'navListContact');
 
 navList.append(navListMenu, navListAbout, navListContact);
 navContainer.appendChild(navList);
 nav.append(h1, navContainer);
 div.append(nav, divMain);
 
-//navbar interaction
-navListMenu.addEventListener("click", () => {
-  const item = document.getElementById("navListMenu");
-  document.getElementById('navListAbout').classList.remove("select-item");
-  document.getElementById('navListContact').classList.remove("select-item");
-  item.setAttribute("class", "select-item")
-  document.getElementById('mainContent').innerHTML = "";
+// navbar interaction
+navListMenu.addEventListener('click', () => {
+  const item = document.getElementById('navListMenu');
+  document.getElementById('navListAbout').classList.remove('select-item');
+  document.getElementById('navListContact').classList.remove('select-item');
+  item.setAttribute('class', 'select-item');
+  document.getElementById('mainContent').innerHTML = '';
   menu();
   footer();
 });
 
-navListAbout.addEventListener("click", () => {
-  const item = document.getElementById("navListAbout");
-  document.getElementById('navListMenu').classList.remove("select-item");
-  document.getElementById('navListContact').classList.remove("select-item");
-  item.setAttribute("class", "select-item")
-  document.getElementById('mainContent').innerHTML = "";
+navListAbout.addEventListener('click', () => {
+  const item = document.getElementById('navListAbout');
+  document.getElementById('navListMenu').classList.remove('select-item');
+  document.getElementById('navListContact').classList.remove('select-item');
+  item.setAttribute('class', 'select-item');
+  document.getElementById('mainContent').innerHTML = '';
   about();
 });
 
-navListContact.addEventListener("click", () => {
-  const item = document.getElementById("navListContact");
-  document.getElementById('navListAbout').classList.remove("select-item");
-  document.getElementById('navListMenu').classList.remove("select-item");
-  item.setAttribute("class", "select-item")
-  document.getElementById('mainContent').innerHTML = "";
+navListContact.addEventListener('click', () => {
+  const item = document.getElementById('navListContact');
+  document.getElementById('navListAbout').classList.remove('select-item');
+  document.getElementById('navListMenu').classList.remove('select-item');
+  item.setAttribute('class', 'select-item');
+  document.getElementById('mainContent').innerHTML = '';
   contact();
   footer();
 });
 
-navListMenu.setAttribute("class", "select-item");
+navListMenu.setAttribute('class', 'select-item');
 menu();
 footer();
