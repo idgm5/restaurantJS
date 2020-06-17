ffdecaa120b1673df5cee32eb6a8b6e1074de980
idgm5 <idgm@pm.me>
2020-06-17 10:57:49 -0500

    Add contact page including CSS, Montserrat font

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,6 @@
 
 const contact = () => {
+
   // contact items
   const div = document.getElementById("mainContent");
   const divContact = document.createElement("DIV");
@@ -26,14 +27,21 @@ const contact = () => {
   const secondAddressLine = document.createElement("P");
 
   const iframe = document.createElement("iframe");
-  //contact page
-  h2.innerHTML = "GIVE US A CALL";
-  para.innerHTML = "Let's stay in touch, you are part of the family."
+
+  //contact attributes
+  divContact.setAttribute("class", "contact");
+  divContainer.setAttribute("class", "contact-container");
+  divEmail.setAttribute("class", "contact-collection");
+  divPhone.setAttribute("class", "contact-collection");
+  divAddress.setAttribute("class", "contact-collection");
 
   iframe.setAttribute("src", "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12085.156249140793!2d-73.9794010640625!3d40.77765884446539!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x505d79b92e16a75e!2sNew-York%20Historical%20Society!5e0!3m2!1ses-419!2smx!4v1592406417731!5m2!1ses-419!2smx");
   iframe.setAttribute("width", "600");
   iframe.setAttribute("height", "450");
 
+  //contact content
+  h2.innerHTML = "GIVE US A CALL";
+  para.innerHTML = "Let's stay in touch, you are part of the family."
   email.innerHTML = "EMAIL";
   phone.innerHTML = "PHONE";
   address.innerHTML = "ADDRESS";
@@ -45,6 +53,7 @@ const contact = () => {
   firstAddressLine.innerHTML = "170 Central Park West"
   secondAddressLine.innerHTML = "New York, NY 10024"
 
+  //contact page
   emailCollection.append(firstEmail, secondEmail);
   phoneCollection.append(firstPhone, secondPhone);
   addressCollection.append(firstAddressLine, secondAddressLine);
@@ -56,6 +65,7 @@ const contact = () => {
   divContainer.append(divEmail,divPhone, divAddress);
   divContact.append(h2,para,divContainer, iframe);
   div.appendChild(divContact);
+  
 }
 
 export default contact;
